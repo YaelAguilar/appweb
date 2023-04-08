a81451c2039421bd41646288c52525689ab06859
YaelAguilar <213452@ids.upchiapas.edu.mx>
2023-04-08 14:07:45 -0600

    [ADD] spelling

--- src/data/data.js
+++ src/data/data.js
@@ -4,7 +4,7 @@ export let data = {
     lastName: ["Cruz Aguilar"],
     level: ["Desarrollador"],
     contact: ["213452@ids.upchiapas.edu.mx","+52 966-127-0336","yael_k28"],
-    schools: ["Universidad Politecnica de Chiapas"],
+    schools: ["Universidad Politecnica de Chiapas."],
     skills: ["Java","c++","css","html"],
     experience: ["Trabajos escolares y algunos proyectos personales, principalmente en el paradigma de progrmación orientada a objetos y algunos proyectos web."],
     languages: ["Español","Inglés (intermedio)"],
